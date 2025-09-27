80a026259f954871cf0b6d2826752dd131a3bff5
Filip Gavrilovski <filipgavrilovski28@gmail.com>
2025-09-27 15:39:55 +0200

    init

---
+++ product-service/src/main/java/com/github/gavro081/productservice/ProductServiceApplication.java
@@ -0,0 +1,22 @@
+package com.github.gavro081.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+}
+
+@RestController
+class UserController {
+	@GetMapping("/")
+	String getIndex(){
+		return "hello from product service";
+	}
+}
---
+++ user-service/src/main/java/com/github/gavro081/userservice/UserServiceApplication.java
@@ -0,0 +1,22 @@
+package com.github.gavro081.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
+@RestController
+class UserController {
+	@GetMapping("/")
+	String getIndex(){
+		return "hello from user service";
+	}
+}
